9c8207a654e5f592515ecbf2ac740aa88c8ba575
vino-project2025 <vinoforsundar@gmail.com>
2026-06-25 13:56:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-import streamlit as st
 
-# Title
-st.title("My First Streamlit App")
-
-# Text
-st.write("Welcome to Streamlit!")
-
-# Input box
-name = st.text_input("Enter your name")
-
-# Button
-if st.button("Submit"):
-    st.success("Hello " + name)
